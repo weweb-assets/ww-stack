cd6dd844e90f3426c4a4ccef6cd0c670e7c8483b
Alexis JC <alexisjc.dev@gmail.com>
2022-07-15 11:24:02 +0200

    feat: add repeated list inside binding context (#2)

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -14,7 +14,7 @@
     </template>
     <template #item="{ element, index: itemIndex }">
       <div>
-        <wwLayoutItemContext :index="itemIndex" :item="null" is-repeat :data="element">
+        <wwLayoutItemContext :index="itemIndex" :item="null" is-repeat :data="element" :repeated-list="internalItems">
           <wwLayout path="itemElement"></wwLayout>
         </wwLayoutItemContext>
       </div>
