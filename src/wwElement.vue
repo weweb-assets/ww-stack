1db39a363f06112bd62c49e0a8dfcd7e5cf2d3c2
Aurélie VIOLETTE <orwel.violette@gmail.com>
2023-11-08 17:09:14 +0100

    Disable stack + add handle

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,151 +1,186 @@
 <template>
-  <draggable 
-    v-model="internalItems" 
-    :item-key="itemKey"
-    :clone="el => el"
-    :group="group" 
-    :sort="sortable"
-    :disabled="isEditing"
-    @change="onChange"
-    @start="setDrag(true)"
-    @end="setDrag(false)"
-  >
-    <template #header>
-      <wwLayout path="headerElement"></wwLayout>
-    </template>
-    <template #item="{ element, index: itemIndex }">
-      <div class="draggable-item">
-        <wwLayoutItemContext :index="itemIndex" :item="null" is-repeat :data="element" :repeated-items="internalItems">
-          <wwLayout path="itemElement"></wwLayout>
-        </wwLayoutItemContext>
-      </div>
-    </template>
-    <template #footer>
-      <wwLayout path="footerElement"></wwLayout>
-    </template>
-  </draggable>
+    <draggable
+        v-model="internalItems"
+        :item-key="itemKey"
+        :clone="(el) => el"
+        :group="group"
+        :sort="sortable"
+        :handle="handle?.length ? `.${handle}` : null"
+        :disabled="isEditing || isReadonly"
+        @change="onChange"
+        @start="setDrag(true)"
+        @end="setDrag(false)"
+    >
+        <template #header>
+            <wwLayout path="headerElement"></wwLayout>
+        </template>
+        <template #item="{ element, index: itemIndex }">
+            <div class="draggable-item">
+                <wwLayoutItemContext
+                    :index="itemIndex"
+                    :item="null"
+                    is-repeat
+                    :data="element"
+                    :repeated-items="internalItems"
+                >
+                    <wwLayout path="itemElement"></wwLayout>
+                </wwLayoutItemContext>
+            </div>
+        </template>
+        <template #footer>
+            <wwLayout path="footerElement"></wwLayout>
+        </template>
+    </draggable>
 </template>
 
 <script>
-import draggable from 'vuedraggable';
+import draggable from "vuedraggable";
 
 export default {
-  components: {
-    draggable,
-  },
-  inject: {
-    customHandler: {defaultValue: null},
-    customDragHandler: {defaultValue: null}
-  },
-  props: {
-    wwElementState: { type: Object, required: true },
-    content: { type: Object, required: true },
-    uid: { type: String, required: true },
-    /* wwEditor:start */
-    wwEditorState: { type: Object, required: true },
-    /* wwEditor:end */
-  },
-  emits: ['trigger-event'],
-  setup(props) {
-    const { value: isDragging, setValue: setDrag } = wwLib.wwVariable.useComponentVariable({
-      uid: props.uid,
-      name: 'isDragging',
-      type: 'boolean',
-      defaultValue: false,
-      readonly: true,
-    });
-    return { isDragging, setDrag };
-  },
-  data: () => ({
-    internalItems: [],
-  }),
-  methods: {
-    onChange(change) {
-      this.customHandler && this.customHandler(change, {...this.wwElementState.props, updatedStackItems: this.internalItems})
-      if (change.moved) {
-        this.$emit('trigger-event', { 
-          name: 'item:moved', 
-          event: { 
-            item: change.moved.element,
-            oldIndex: change.moved.oldIndex,
-            newIndex: change.moved.newIndex,
-            updatedList: this.internalItems
-          }
-        })
-      }
+    components: {
+        draggable,
+    },
+    inject: {
+        customHandler: { defaultValue: null },
+        customDragHandler: { defaultValue: null },
+    },
+    props: {
+        wwElementState: { type: Object, required: true },
+        content: { type: Object, required: true },
+        uid: { type: String, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
+    },
+    emits: ["trigger-event"],
+    setup(props) {
+        const { value: isDragging, setValue: setDrag } = wwLib.wwVariable.useComponentVariable({
+            uid: props.uid,
+            name: "isDragging",
+            type: "boolean",
+            defaultValue: false,
+            readonly: true,
+        });
+        return { isDragging, setDrag };
+    },
+    data: () => ({
+        internalItems: [],
+    }),
+    methods: {
+        onChange(change) {
+            this.customHandler &&
+                this.customHandler(change, { ...this.wwElementState.props, updatedStackItems: this.internalItems });
+            if (change.moved) {
+                this.$emit("trigger-event", {
+                    name: "item:moved",
+                    event: {
+                        item: change.moved.element,
+                        oldIndex: change.moved.oldIndex,
+                        newIndex: change.moved.newIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
 
-      if (change.added) {
-        this.$emit('trigger-event', {
-          name: 'item:added',
-          event: {
-            item: change.added.element,
-            newIndex: change.added.newIndex,
-            updatedList: this.internalItems
-          }
-        })
-      }
+            if (change.added) {
+                this.$emit("trigger-event", {
+                    name: "item:added",
+                    event: {
+                        item: change.added.element,
+                        newIndex: change.added.newIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
 
-      if (change.removed) {
-        this.$emit('trigger-event', {
-          name: 'item:removed',
-          event: {
-            item: change.removed.element,
-            oldIndex: change.removed.oldIndex,
-            updatedList: this.internalItems
-          }
-        })
-      }
-    }
-  },
-  computed: {
-    isEditing() {
-      /* wwEditor:start */
-      return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
-      /* wwEditor:end */
-      // eslint-disable-next-line no-unreachable
-      return false;
+            if (change.removed) {
+                this.$emit("trigger-event", {
+                    name: "item:removed",
+                    event: {
+                        item: change.removed.element,
+                        oldIndex: change.removed.oldIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
+        },
     },
-    items() {
-      const data = this.wwElementState.props.items ? this.wwElementState.props.items : this.content.items
-      const items = wwLib.wwCollection.getCollectionData(data)
-      if (!Array.isArray(items)) return []
-      return items
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        items() {
+            const data = this.wwElementState.props.items ? this.wwElementState.props.items : this.content.items;
+            const items = wwLib.wwCollection.getCollectionData(data);
+            if (!Array.isArray(items)) return [];
+            return items;
+        },
+        group() {
+            return this.wwElementState.props.group ? this.wwElementState.props.group : this.content.group;
+        },
+        sortable() {
+            return this.wwElementState.props.sortable ? this.wwElementState.props.sortable : this.content.sortable;
+        },
+        itemKey() {
+            return this.wwElementState.props.itemKey || "id";
+        },
+        handle() {
+            return this.wwElementState.props.handle?.length ? this.wwElementState.props.handle : this.content.handle;
+        },
+        isReadonly() {
+            /* wwEditor:start */
+            if (this.wwEditorState.isSelected) {
+                return this.wwElementState.states.includes("readonly");
+            }
+            /* wwEditor:end */
+            return this.wwElementState.props.readonly || this.content.readonly;
+        },
     },
-    group() {
-      return this.wwElementState.props.group ? this.wwElementState.props.group : this.content.group
+    watch: {
+        items: {
+            immediate: true,
+            handler: function (value) {
+                this.internalItems = [...value];
+            },
+        },
+        isDragging(value) {
+            if (this.customDragHandler) {
+                this.customDragHandler(value, { ...this.wwElementState.props });
+            }
+        },
+        /* wwEditor:start */
+        isReadonly: {
+            immediate: true,
+            handler(value) {
+                if (value) {
+                    this.$emit("add-state", "readonly");
+                } else {
+                    this.$emit("remove-state", "readonly");
+                }
+
+                this.$nextTick(() => {
+                    this.handleObserver();
+                });
+            },
+        },
+        /* wwEditor:end */
     },
-    sortable() {
-      return this.wwElementState.props.sortable ? this.wwElementState.props.sortable : this.content.sortable
-    },
-    itemKey() {
-      return this.wwElementState.props.itemKey || 'id'
-    }
-  },
-  watch: {
-    items: {
-      immediate: true,
-      handler: function (value) {
-        this.internalItems = [...value]
-      }
-    },
-    isDragging(value) {
-      if(this.customDragHandler) {
-        this.customDragHandler(value, {...this.wwElementState.props})
-      }
-    }
-  }
 };
 </script>
 
 <style scoped>
 /** FIX POINTER-EVENTS: ALL BREAKING DRAGGABLE ON MOBILE/TABLET (TOUCH MODE) */
 .draggable-item :deep(.ww-layout) {
-  pointer-events: unset!important;
+    pointer-events: unset !important;
 }
 .draggable-item :deep(* > .ww-object) {
-  pointer-events: unset!important;
+    pointer-events: unset !important;
 }
 .draggable-item :deep(** > .ww-object) {
-  pointer-events: unset!important;
+    pointer-events: unset !important;
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,93 +1,119 @@
 export default {
-  editor: {
-    label: {
-      en: 'Stack',
+    editor: {
+        label: {
+            en: "Stack",
+        },
+        icon: "inbox-in",
     },
-    icon: 'inbox-in'
-  },
-  triggerEvents: [
-    { 
-      name: 'item:moved',
-      label: { en: 'On item moved' },
-      event: { 
-        item: {},
-        oldIndex: 0,
-        newIndex: 1,
-        updatedList: []
-      }
+    triggerEvents: [
+        {
+            name: "item:moved",
+            label: { en: "On item moved" },
+            event: {
+                item: {},
+                oldIndex: 0,
+                newIndex: 1,
+                updatedList: [],
+            },
+        },
+        {
+            name: "item:added",
+            label: { en: "On item added" },
+            event: {
+                item: {},
+                newIndex: 1,
+                updatedList: [],
+            },
+        },
+        {
+            name: "item:removed",
+            label: { en: "On item removed" },
+            event: {
+                item: {},
+                oldIndex: 0,
+                updatedList: [],
+            },
+        },
+    ],
+    properties: {
+        group: {
+            hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.group),
+            label: {
+                en: "Group",
+            },
+            type: "Text",
+            bindable: true,
+            defaultValue: "common",
+            section: "settings",
+        },
+        items: {
+            hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.items),
+            label: {
+                en: "Items",
+            },
+            type: "Info",
+            options: {
+                text: { en: "Bind your data" },
+            },
+            bindable: true,
+            defaultValue: [],
+            section: "settings",
+        },
+        sortable: {
+            hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.sortable),
+            label: {
+                en: "Sortable",
+            },
+            type: "OnOff",
+            defaultValue: true,
+            section: "settings",
+        },
+        itemElement: {
+            hidden: true,
+            defaultValue: [{ isWwObject: true, type: "ww-flexbox" }],
+            navigator: {
+                group: "Item",
+            },
+        },
+        headerElement: {
+            hidden: true,
+            defaultValue: [{ isWwObject: true, type: "ww-text" }],
+            navigator: {
+                group: "Header",
+            },
+        },
+        footerElement: {
+            hidden: true,
+            defaultValue: [],
+            navigator: {
+                group: "Footer",
+            },
+        },
+        readonly: {
+            label: { en: "Read only", fr: "Lecture seule" },
+            type: "OnOff",
+            section: "settings",
+            bindable: true,
+            defaultValue: false,
+            /* wwEditor:start */
+            bindingValidation: {
+                type: "boolean",
+                tooltip: "A boolean that defines if the input is in readonly: `true | false`",
+            },
+            /* wwEditor:end */
+        },
+        handle: {
+            label: "Handle class",
+            type: "Text",
+            bindable: true,
+            section: "settings",
+            hidden: (content, sidePanelContent, boundProps, wwProps) => wwProps && wwProps.handle?.length,
+            /* wwEditor:start */
+            bindingValidation: {
+                type: "string",
+                tooltip: "A string that represent the class of the handle",
+            },
+            /* wwEditor:end */
+        },
     },
-    { 
-      name: 'item:added',
-      label: { en: 'On item added' },
-      event: { 
-        item: {},
-        newIndex: 1,
-        updatedList: []
-      }
-    },
-    { 
-      name: 'item:removed',
-      label: { en: 'On item removed' },
-      event: { 
-        item: {},
-        oldIndex: 0,
-        updatedList: []
-      }
-    },
-  ],
-  properties: {
-    group: {
-      hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.group),
-      label: {
-        en: 'Group'
-      },
-      type: 'Text',
-      bindable: true,
-      defaultValue: 'common',
-      section: 'settings'
-    },
-    items: {
-      hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.items),
-      label: {
-        en: 'Items',
-      },
-      type: 'Info',
-      options: {
-        text: { en: 'Bind your data' }
-      },
-      bindable: true,
-      defaultValue: [],
-      section: 'settings',
-    },
-    sortable: {
-      hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.sortable),
-      label: {
-        en: 'Sortable',
-      },
-      type: 'OnOff',
-      defaultValue: true,
-      section: 'settings',
-    },
-    itemElement: {
-      hidden: true,
-      defaultValue: [{ isWwObject: true, type: 'ww-flexbox' }],
-      navigator: {
-        group: 'Item',
-      },
-    },
-    headerElement: {
-      hidden: true,
-      defaultValue: [{ isWwObject: true, type: 'ww-text' }],
-      navigator: {
-        group: 'Header',
-      },
-    },
-    footerElement: {
-      hidden: true,
-      defaultValue: [],
-      navigator: {
-        group: 'Footer',
-      },
-    },
-  },
 };
